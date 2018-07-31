10bacc891934549ded6cfe6fe1fc066375b0c34c
Richard Chen <richard.c.chen@gmail.com>
2018-07-31 20:02:36 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -8,7 +8,7 @@ def run_guessing_game
   
   if userinput == "exit"
     puts "Goodbye!"
-  elsif userinput == dice
+  elsif userinput.to_i == dice
     puts "You guessed the correct number!"
   else
     puts "The computer guessed #{dice}."
