29e6d0814246c20851581d82fc8b97f451ca7e7c
Richard Chen <richard.c.chen@gmail.com>
2018-07-31 20:07:02 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,16 +1,17 @@
 require 'pry'
 # Code your solution helper_method
 def run_guessing_game
-  puts "Guess a number between 1 and 6."
-  dice = rand(1..6)
-  puts dice
-  userinput = gets.chomp
+ until userinput == "exit"
+    puts "Guess a number between 1 and 6."
+    dice = rand(1..6)
+    userinput = gets.chomp
   
-  if userinput == "exit"
-    puts "Goodbye!"
-  elsif userinput == dice
-    puts "You guessed the correct number!"
-  else
-    puts "The computer guessed #{dice}."
+    if userinput == "exit"
+      puts "Goodbye!"
+    elsif userinput == dice
+      puts "You guessed the correct number!"
+    else
+      puts "The computer guessed #{dice}."
+    end
   end
 end
