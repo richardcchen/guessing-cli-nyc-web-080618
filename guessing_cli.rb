f3bc5ad4431e0a61ab056238bb444a0a9c540de6
Richard Chen <richard.c.chen@gmail.com>
2018-07-31 20:56:02 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,6 +1,7 @@
 require 'pry'
 # Code your solution helper_method
 def run_guessing_game
+  userinput = " "
   while userinput != "exit"
     puts "Guess a number between 1 and 6."
     dice = rand(1..6)
